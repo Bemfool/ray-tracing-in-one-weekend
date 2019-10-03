b788beb1481574a9362ad9007ad96b4778995356
Keith Lin <3160104229@zju.edu.cn>
2019-10-03 16:36:23 +0800

    fix inline

--- 3-the-vec3-class/vec3.cpp
+++ 3-the-vec3-class/vec3.cpp
@@ -1,49 +1,49 @@
 ﻿#include "vec3.h"
 
-inline std::istream& operator>>(std::istream &is, vec3 &t) {
+std::istream& operator>>(std::istream &is, vec3 &t) {
 	is >> t.e[0] >> t.e[1] >> t.e[2];
 	return is;
 }
-inline std::ostream& operator<<(std::ostream &os, const vec3 &t) {
+std::ostream& operator<<(std::ostream &os, const vec3 &t) {
 	os << t.e[0] << " " << t.e[1] << " " << t.e[2];
 	return os;
 }
-inline void vec3::make_unit_vector() {
+void vec3::make_unit_vector() {
 	float k = 1.0 / sqrt(e[0] * e[0] + e[1] * e[1] + e[2] * e[2]);
 	e[0] *= k; e[1] *= k; e[2] *= k;
 }
 
-inline vec3& vec3::operator+=(const vec3 &v) {
+vec3& vec3::operator+=(const vec3 &v) {
 	e[0] += v.e[0];
 	e[1] += v.e[1];
 	e[2] += v.e[2];
 	return *this;
 }
-inline vec3& vec3::operator-=(const vec3& v) {
+vec3& vec3::operator-=(const vec3& v) {
 	e[0] -= v.e[0];
 	e[1] -= v.e[1];
 	e[2] -= v.e[2];
 	return *this;
 }
-inline vec3& vec3::operator*=(const vec3 &v) {
+vec3& vec3::operator*=(const vec3 &v) {
 	e[0] *= v.e[0];
 	e[1] *= v.e[1];
 	e[2] *= v.e[2];
 	return *this;
 }
-inline vec3& vec3::operator*=(const float t) {
+vec3& vec3::operator*=(const float t) {
 	e[0] *= t;
 	e[1] *= t;
 	e[2] *= t;
 	return *this;
 }
-inline vec3& vec3::operator/=(const vec3 &v) {
+vec3& vec3::operator/=(const vec3 &v) {
 	e[0] /= v.e[0];
 	e[1] /= v.e[1];
 	e[2] /= v.e[2];
 	return *this;
 }
-inline vec3& vec3::operator/=(const float t) {
+vec3& vec3::operator/=(const float t) {
 	float k = 1.0 / t;
 	e[0] *= k;
 	e[1] *= k;
